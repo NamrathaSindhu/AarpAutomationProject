a544b53c19a37cb182bb0716cc03dce973139a74
NamrathaSindhu <73485626+NamrathaSindhu@users.noreply.github.com>
2020-10-29 11:17:54 +0530

    Update Company.java

--- src/employee/Company.java
+++ src/employee/Company.java
@@ -2,10 +2,12 @@ package employee;
 
 public abstract class Company {
 
+	public int id;
 	public String companyName;
-	public Company(String cn)
+	public Company(String cn,String id)
 	{
 		this.companyName=cn;
+		this.id=id;
 	}
 	
 }
