c5ff98e7a4d2f6f33bb9e71e6f22caf3094c530b
NamrathaSindhu <dolanam.pr@gmail.com>
2020-10-29 11:42:00 +0530

    Added employee details

---
+++ src/employee/AddDetails.java
@@ -0,0 +1,18 @@
+package employee;
+
+import java.util.Scanner;
+
+public class AddDetails {
+
+	public static void main(String[] args) {
+
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter employee name");
+		String str=sc.nextLine();
+		System.out.println(" enter employee age");
+		int age=sc.nextInt();
+		System.out.println("enter employee height");
+		float height=sc.nextFloat();
+	}
+
+}
